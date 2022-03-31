a0a51044f9264ffac7fd2a8ae4491540b246d325
Yash-Daxini <100989835+Yash-Daxini@users.noreply.github.com>
2022-03-31 23:41:48 +0530

    Lab-9

--- StudentDetailsPro.java
+++ StudentDetailsPro.java
@@ -8,10 +8,11 @@ class StudentDetails{
     public void getStudentDetails(){
         System.out.println("\t----Enter Student Details----");
         Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
         System.out.print("Enter Enrollment Number:");
         this.Enrollment_No = sc.nextInt();
         System.out.print("Enter Name:");
-        this.Name = sc.next();
+        this.Name = sc1.nextLine();
         System.out.print("Enter Semester:");
         this.Semester = sc.nextInt();
         System.out.print("Enter CPI:");
@@ -28,17 +29,24 @@ class StudentDetails{
 }
 class StudentDetailsPro{
     public static void main(String[] args) {
-        StudentDetails s1 = new StudentDetails();
-        StudentDetails s2 = new StudentDetails();
-        StudentDetails s3 = new StudentDetails();
-        StudentDetails s4 = new StudentDetails();
-        StudentDetails s5 = new StudentDetails();
+        // StudentDetails s1 = new StudentDetails();
+        // StudentDetails s2 = new StudentDetails();
+        // StudentDetails s3 = new StudentDetails();
+        // StudentDetails s4 = new StudentDetails();
+        // StudentDetails s5 = new StudentDetails();
 
-        StudentDetails [] students = {s1,s2,s3,s4,s5};
-        
-        for(StudentDetails s:students){
-            s.getStudentDetails();
-            s.displayStudentDetails();
+        StudentDetails [] students = new StudentDetails[5];
+
+        // for(StudentDetails s:students){
+        //     s.getStudentDetails();
+        //     s.displayStudentDetails();
+        // }
+        for(int i = 0;i<5;i++){
+            students[i] = new StudentDetails();
+            students[i].getStudentDetails();
+        }
+        for(int i = 0;i<5;i++){
+            students[i].displayStudentDetails();
         }
     }
 }
