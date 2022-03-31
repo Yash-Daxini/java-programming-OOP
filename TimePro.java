b4df5316e383e5bed888c27394d99d40019c8643
Yash-Daxini <100989835+Yash-Daxini@users.noreply.github.com>
2022-03-31 23:40:39 +0530

    Lab-8

--- TimePro.java
+++ TimePro.java
@@ -1,22 +1,26 @@
-import java.util.*;
 class Time{
     private int minute;
     private int hour;
 
-    Time(int givenminute,int givenhour){
+    Time(int givenhour,int givenminute){
         this.minute = givenminute;
         this.hour  = givenhour;
     }
 
     static void TimeObjSum(Time obj1 , Time obj2){
-        System.out.println("Sum of Minutes:"+(obj1.minute + obj2.minute)); 
-        System.out.println("Sum of Hours:"+(obj1.hour + obj2.hour));
+        int summinutes = (obj1.minute + obj2.minute);
+        int sumhours = (obj1.hour + obj2.hour);
+        if(summinutes > 60){
+            sumhours += summinutes/60;
+            summinutes -= (summinutes/60)*60;
+        }
+        System.out.println(sumhours+" : "+summinutes); 
     }
 }
 public class TimePro {
     public static void main(String[] args) {
-        Time t1 = new Time(15,17);
-        Time t2 = new Time(15,12);
+        Time t1 = new Time(1,40);
+        Time t2 = new Time(4,60);
         Time.TimeObjSum(t1 , t2);
     }
 }
